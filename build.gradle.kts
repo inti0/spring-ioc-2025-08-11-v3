8ed3cfb03ce01d65c7810c524e565f2355749621
jhs512 <jangka512@gmail.com>
2025-06-04 09:16:23 +0900

    work

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+}
+
+group = "com"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    testImplementation("org.assertj:assertj-core:3.27.3")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.19.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
